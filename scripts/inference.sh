505ca82d5fde3b07cec11371b8da040bed89a183
Bian-Yichen <3202796115@qq.com>
2026-08-24 18:29:43 +0800

    change

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=6 python scripts/inference_wan_latent_reconstruction.py \
-  --config configs/gim_wan_latent_decoder.json \
-  --checkpoint /mnt/shared-storage-user/bianyichen/lingbot-video-output/gim-wan-latent-decoder-output/checkpoint-iter-00018200-step-00018200 \
+CUDA_VISIBLE_DEVICES=0 python scripts/inference_memory_3drae.py \
+  --config configs/memory_3drae.json \
+  --checkpoint /mnt/shared-storage-user/bianyichen/lingbot-video-output/memory-3drae/checkpoint-iter-00003200-step-00003200 \
   --item_name 9WS0D7j9yxc_024000_029000.mp4 \
-  --output_dir /mnt/shared-storage-user/bianyichen/gim_inference/gim-wan-latent-decoder-iter18200
+  --output_dir /mnt/shared-storage-user/bianyichen/gim_inference/memory-3drae-iter3200
